4ada4cd294099b54ae8b804f0245b3ae43caeb82
Nick Collins <nick@vividx.co.uk>
2013-04-24 17:03:19 -0400

    scrape.py collect repo data

--- scrape.py
+++ scrape.py
@@ -23,7 +23,7 @@ def get_page(url):
         return None
 
 
-def get_pages(url, max_pages):
+def get_pages(url, max_pages=100):
     data = []
     for i in range(max_pages):
         print('GET: {}'.format(url.format(i)))
@@ -40,13 +40,34 @@ if __name__ == '__main__':
         user_page = get_page(user_url.format(person))
         d = {}
         d['login'] = person
-        followers_url = user_page['followers_url']
-        followers_page, pagecount = get_pages(followers_url+'?page={}', 20)
+
+        # followers
+        followers_url = user_page['followers_url'] + '?page={}'
+        followers_json, _ = get_pages(followers_url)
         followers = []
-        for f in followers_page:
-            if f['login'] in hacker_school.people:
-                followers.append(f['login'])
+        for follower in followers_json:
+            if follower['login'] in hacker_school.people:
+                followers.append(follower['login'])
         d['followers'] = followers
+        d['hs_followers_count'] = len(followers)
+
+        # repos
+        repos_url = user_page['repos_url'] + '?page={}'
+        repos_json, _ = get_pages(repos_url)
+        for repo in repos_json:
+            #print(repo)
+            size = repo['size']
+            language = str(repo['language'])
+            fork = repo['fork']
+            forks = repo['forks']
+            if fork == True:
+                d['total_forks'] = d.get('total_forks', 0) + 1
+            else:
+                d['own_repo_size'] = d.get('own_repo_size', 0) + size
+                d['lang:'+language] = d.get('lang:'+language, 0) + 1
+                d['own_repo_count'] = d.get('own_repo_count', 0) + 1
+
+        # add to main json data
         data.append(d)
 
     with open('hackers.json', 'w') as f:
