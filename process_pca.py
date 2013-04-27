64c2e1c557d20abc61ea7a80764732486acda4b0
Nick Collins <nick@vividx.co.uk>
2013-04-27 17:19:33 -0400

    `process_pca.py` now procudes links between nodes
    where Hacker Schoolers have push, commented, raised an issue or created
    a pull request to another Hacker Schoolers repo.

--- process_pca.py
+++ process_pca.py
@@ -9,6 +9,8 @@ from __future__ import absolute_import
 import json
 import numpy as np
 
+import hacker_school
+
 with open('data/hackers.json', 'r') as f:
     data = json.loads(f.read())
 
@@ -53,15 +55,47 @@ v0, v1 = v[ordering[-1]], v[ordering[-2]]
 for i, (f, c) in enumerate(top10):
     print(round(v0[i],1), '\t', round(v1[i],1), '\t', f)
 
-data_2d = []
+data_nodes = []
 for i, p in enumerate(data):
     x = sum(v0 * reduced_array[i])
     y = sum(v1 * reduced_array[i])
     login = p['login']
     url = p['url']
-    data_2d.append({'x': x, 'y': y, 'login': login, 'url': url})
+    data_nodes.append({'x': x, 'y': y, 'login': login, 'url': url})
 
-json_data = json.dumps(data_2d)
+data_all = {'nodes': data_nodes, 'links': []}
+
+#for p1, p2 in zip(data_nodes[:-1], data_nodes[1:]):
+#    data_all['links'].append({'x1': p1['x'],
+#                              'y1': p1['y'],
+#                              'x2': p2['x'],
+#                              'y2': p2['y']})
+
+people = dict([(p['login'], p) for p in data_nodes])
+people_set = set(people.keys())
+
+key_events = set(['PullRequestEvent', 'PushEvent', 'IssueCommentEvent', 'IssuesEvent'])
+
+for person in data:
+    collaborators = set([event['repo']['name'].split('/')[0]
+                         for event in person['events']
+                         if event['type'] in key_events])
+    collaborators.difference_update([person['login']])
+    collaborators.intersection_update(people_set)
+    for c in collaborators:
+        try:
+            link = {'x1': people[person['login']]['x'],
+                    'y1': people[person['login']]['y'],
+                    'x2': people[c]['x'],
+                    'y2': people[c]['y'],
+                   }
+            data_all['links'].append(link)
+        except:
+            # TODO: fix this!
+            pass
+
+
+json_data = json.dumps(data_all)
 
 with open('data/hackers_pca.json', 'w') as f:
     f.write(json_data)
