a1b60030ff08d85d17b9efa8a78b9a11864d8dc4
Nick Collins <nick@vividx.co.uk>
2013-04-30 16:40:01 -0400

    Major refactoring
    Separate functions `SelectHacker`, `drawLinks` and `drawNodes`.
    
    These use accessor functions `xfunc` and `yfunc` to retrive the
    coordinates from data items. This means the axis can be changed without
    modifiying the JSON data file.
    
    The links are now just objects holding `source` and `target` identifiers
    rather than the coordinates.

--- process_pca.py
+++ process_pca.py
@@ -57,40 +57,20 @@ if __name__ == '__main__':
     features.difference_update(['events', 'followers', 'login', 'url', 'repos', 'avatar_url'])
     orderedfeatures = list(features)
 
-    #full_array = np.ndarray((len(data_scraping), len(features)))
-
-    #for row, p in enumerate(scraping_data):
-    #    for col, f in enumerate(orderedfeatures):
-    #        full_array[row, col] = p.get(f, 0)
-
-    #top10 = sorted([(l, int(c)) for l, c in zip(orderedfeatures, np.sum(full_array, 0))
-    #                if l[:5] == 'lang:' and not l == 'lang:None'],
-    #               reverse = True,
-    #               key = lambda pair: pair[1])[:10]
-
-    #reduced_array = np.ndarray((len(scraping_data), 10+1))
-    #for row, p in enumerate(scraping_data):
-    #    all_repos = p.get('own_repo_count', 1)
-    #    for col, (f, c) in enumerate(top10):
-    #        reduced_array[row, col] = p.get(f, 0) #/ all_repos
-    #    # other langs
-    #    reduced_array[row, 10] = all_repos - sum(reduced_array[row, :10])
-    #    reduced_array[row] = reduced_array[row] - all_repos/11
-
-    #reduced_array = reduced_array - np.average(reduced_array, 0)
-
     languages = [f for f in features if f[:5] == 'lang:' and not f == 'lang:None']
     reduced_array, top10 = top_feature_matrix(scraping_data, languages, 10)
 
-    w, v = np.linalg.eig(np.cov(reduced_array.T))
+    W, V = np.linalg.eig(np.cov(reduced_array.T))
 
-    ordering = w.argsort()
+    ordering = W.argsort()
 
-    w0, w1 = w[ordering[-1]], w[ordering[-2]]
-    v0, v1 = v[ordering[-1]], v[ordering[-2]]
+    W0, W1 = W[ordering[-1]], W[ordering[-2]]
+    V0, V1 = V[:, ordering[-1]], V[:, ordering[-2]]
 
     for i, (f, c) in enumerate(top10):
-        print(round(v0[i],1), '\t', round(v1[i],1), '\t', f)
+        print(round(V0[i],1), '\t', round(V1[i],1), '\t', f)
+
+    #
 
     people = dict([(p['login'], p) for p in scraping_data])
     people_set = set(people.keys())
@@ -98,12 +78,12 @@ if __name__ == '__main__':
 
     data_nodes = []
     for i, p in enumerate(scraping_data):
-        x = sum(v0 * reduced_array[i])
-        y = sum(v1 * reduced_array[i])
+        pc0 = sum(V0 * reduced_array[i])
+        pc1 = sum(V1 * reduced_array[i])
         login = p['login']
         url = p['url']
         avatar_url = p.get('avatar_url', '')
-        data_nodes.append({'x': x, 'y': y, 'login': login, 'url': url,
+        data_nodes.append({'pc0': pc0, 'pc1': pc1, 'login': login, 'url': url,
                            'avatar_url': avatar_url, 'id': people_numbers[login]})
 
     data_all = {'nodes': data_nodes, 'links': []}
@@ -124,18 +104,14 @@ if __name__ == '__main__':
         collaborators.intersection_update(people_set)
         for c in collaborators:
             try:
-                link = {'x1': people[person['login']]['x'],
-                        'y1': people[person['login']]['y'],
-                        'x2': people[c]['x'],
-                        'y2': people[c]['y'],
-                        'source': people_numbers[person['login']],
-                        'target': people_numbers[c],
-                       }
+                link = {'source': people_numbers[person['login']],
+                        'target': people_numbers[c]}
                 data_all['links'].append(link)
             except Exception as e:
-                print(e)
                 # TODO: fix this!
                 pass
+
+    # output data
 
     json_data = json.dumps(data_all, indent=1)
 
--- website/js/pca_graph.js
+++ website/js/pca_graph.js
@@ -1,84 +1,26 @@
+
 function draw(data) {
     "use strict";
 
-    // helper functions
-    
-   function drawLinks(links) {
-
-        var lines = d3.select('svg')
-            .selectAll('line')
-            .data([])
-            .exit()
-            .remove();
-
-        var lines = d3.select('svg')
-            .selectAll('line')
-            .data(links)
-            .enter()
-            .append('svg:line')
-            .attr('x1', function(d) {return x_scale(d.x1)})
-            .attr('y1', function(d) {return y_scale(d.y1)})
-            .attr('x2', function(d) {return x_scale(d.x2)})
-            .attr('y2', function(d) {return y_scale(d.y2)})
-            .style('stroke', 'rgb(0, 52, 72)')
-            .style('alpha', 1)
-            .style('stroke-width', 1);
-    } 
-   
-    function selectHacker(hacker) {
-        var id = hacker.id;
-        var filteredlinks = _.filter(data.links,
-                function(l) {
-                    return l.source == id || l.target == id
-                });
-
-        drawLinks(filteredlinks);
-
-        var hackerIDs = _.union(
-                _.map(filteredlinks, function(l) {return l.source}),
-                _.map(filteredlinks, function(l) {return l.target}));
-
-        var hackers = _.map(
-                hackerIDs,
-                function(n) {return data.nodes[n]});
-
-        d3.select('#collaborators')
-        .selectAll('div')
-        .data([])
-        .exit()
-        .remove();
-
-        var divs = d3.select('#collaborators')
-        .selectAll('div')
-        .data(hackers)
+    // pictures
+    d3.select('#hackers')
+        .selectAll('img')
+        .data(data.nodes)
         .enter()
-        .append('div')
-        .attr('class', 'collaborator')
-        .attr('height', '40px')
-        .attr('width', '200px')
-
-        divs.append('img')
-        .attr('class', 'collaborator-image')
+        .append('img')
         .attr('height', '30px')
         .attr('width', '30px')
-        .attr('vertical-align', 'center')
         .attr('src', function(d) {return d.avatar_url})
         .attr('title', function(d) {return d.login})
-        
-        divs.append('div')
-        .attr('class', 'collaborator-name')
-        .append('span')
-        .text(function(d) {return d.login});
-
-    }
+        //.on('click', function(x) {x} ); //selectHacker); 
+        .on('click', function(d) {selectHacker(d, data.links, data.nodes, xfunc, yfunc)}); 
 
     // main body
-
     var margin = 50,
         height = 400,
         width = 600,
-        x_extent = d3.extent(data.nodes, function(d) {return d.x}),
-        y_extent = d3.extent(data.nodes, function(d) {return d.y});
+        x_extent = d3.extent(data.nodes, function(d) { return d.pc0 }),
+        y_extent = d3.extent(data.nodes, function(d) { return d.pc1 });
 
     var x_scale = d3.scale.sqrt()
         .range([margin, width-margin])
@@ -88,10 +30,13 @@ function draw(data) {
         .range([height-margin, margin]) // margin-height to reverse direction
         .domain(y_extent);
 
+    var xfunc = function(d) {return x_scale(d.pc0)};
+    var yfunc = function(d) {return y_scale(d.pc1)};
+
     d3.select('#graph')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
 
     var x_axis = d3.svg.axis().scale(x_scale);
     var y_axis = d3.svg.axis().scale(y_scale).orient('left');
@@ -108,35 +53,100 @@ function draw(data) {
     .attr('transform', 'translate(' + margin + ',0)')
     .call(y_axis);
 
-    drawLinks(data.links);
+    // nodes
+    drawNodes(data, xfunc, yfunc);
+
+}
+
+
+function selectHacker(hacker, links, nodes, xfunc, yfunc) {
+
+    var id = hacker.id;
+    var filteredlinks = _.filter(links,
+            function(l) {
+                return l.source == id || l.target == id;
+            });
+
+    drawLinks(filteredlinks, nodes, xfunc, yfunc);
+
+    var hackerIDs = _.union(
+            _.map(filteredlinks, function(l) {return l.source}),
+            _.map(filteredlinks, function(l) {return l.target}));
+
+    var hackers = _.map(
+            hackerIDs,
+            function(n) {return nodes[n]});
+
+    d3.select('#collaborators')
+        .selectAll('div')
+        .data([])
+        .exit()
+        .remove();
+
+    var divs = d3.select('#collaborators')
+        .selectAll('div')
+        .data(hackers)
+        .enter()
+        .append('div')
+        .attr('class', 'collaborator')
+        .attr('height', '40px')
+        .attr('width', '200px')
+
+        divs.append('img')
+        .attr('class', 'collaborator-image')
+        .attr('height', '30px')
+        .attr('width', '30px')
+        .attr('vertical-align', 'center')
+        .attr('src', function(d) {return d.avatar_url})
+        .attr('title', function(d) {return d.login})
+
+        divs.append('div')
+        .attr('class', 'collaborator-name')
+        .append('span')
+        .text(function(d) {return d.login});
+
+}
+
+
+function drawLinks(links, nodes, xfunc, yfunc) {
+
+    var lines = d3.select('svg')
+        .selectAll('line')
+        .data([])
+        .exit()
+        .remove();
+
+    var lines = d3.select('svg')
+        .selectAll('line')
+        .data(links)
+        .enter()
+        .append('svg:line')
+        .attr('x1', function(d) {return xfunc(nodes[d.source])})
+        .attr('y1', function(d) {return yfunc(nodes[d.source])})
+        .attr('x2', function(d) {return xfunc(nodes[d.target])})
+        .attr('y2', function(d) {return yfunc(nodes[d.target])})
+        .style('stroke', 'rgb(0, 52, 72)')
+        .style('alpha', 1)
+        .style('stroke-width', 1);
+
+} 
+
+
+function drawNodes(data, xfunc, yfunc) {
 
     d3.select('svg')
         .selectAll('circle')
         .data(data.nodes)
         .enter()
         .append('circle')
-        .attr('cx', function(d) {return x_scale(d.x)})
-        .attr('cy', function(d) {return y_scale(d.y)})
+        .attr('cx', function(d) {return xfunc(d)})
+        .attr('cy', function(d) {return yfunc(d)})
         .attr('r', function(d) {return 6})
         .append("svg:title")
         .text(function(d) { return d.login; })
         .attr("xlink:href", function(d) {
             return d.url
         });
-
-
-    // pictures
-
-    d3.select('#hackers')
-    .selectAll('img')
-    .data(data.nodes)
-    .enter()
-    .append('img')
-    .attr('height', '30px')
-    .attr('width', '30px')
-    .attr('src', function(d) {return d.avatar_url})
-    .attr('title', function(d) {return d.login})
-    .on('click', selectHacker);
 
 }
 
