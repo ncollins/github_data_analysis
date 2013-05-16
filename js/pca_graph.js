f5de0a5a61c4fbe813d7eebd9f2aaa78a9eddb5b
Nick Collins <nick@vividx.co.uk>
2013-05-16 00:32:12 -0400

    Changes recomended by JSLint

--- js/pca_graph.js
+++ js/pca_graph.js
@@ -1,4 +1,7 @@
+var d3 = d3;
+
 function setup(data) {
+    'use strict';
 
     d3.select('#hackers')
         .selectAll('img')
@@ -7,27 +10,27 @@ function setup(data) {
         .append('img')
         .attr('height', '30px')
         .attr('width', '30px')
-        .attr('src', function(d) {return d.avatar_url})
-        .attr('title', function(d) {return d.login})
+        .attr('src', function (d) { return d.avatar_url; })
+        .attr('title', function (d) { return d.login; });
 
     d3.select('#pcbutton')
-        .on('click', function(d) {
+        .on('click', function (d) {
             d3.json("data/hacker_links.json",
-                function(d) {
+                function (d) {
                     draw(d, 'pc0', 'pc1', 'Python', 'Javascript');
-                })
-            });
+                });
+        });
 
     d3.select('#contributionsbutton')
-        .on('click', function(d) {
+        .on('click', function (d) {
             d3.json("data/hacker_links.json",
-                function(d) {
+                function (d) {
                     draw(d, 'total_collaboration', 'hs_collaboration',
                         'Total Collaboration', 'Hacker School Collaboration');
-                })
-            });
+                });
+        });
 
-     draw(data, 'pc0', 'pc1', 'Python', 'Javascript') 
+    draw(data, 'pc0', 'pc1', 'Python', 'Javascript');
 
 }
 
@@ -39,21 +42,21 @@ function draw(data, xval, yval, xlab, ylab) {
     var margin = 50,
         height = 400,
         width = 600,
-        x_extent = d3.extent(data.nodes, function(d) { return d[xval] }),
-        y_extent = d3.extent(data.nodes, function(d) { return d[yval] });
+        x_extent = d3.extent(data.nodes, function (d) { return d[xval]; }),
+        y_extent = d3.extent(data.nodes, function (d) { return d[yval]; });
 
     var x_scale = d3.scale.sqrt()
-        .range([margin, width-margin])
+        .range([margin, width - margin])
         .domain(x_extent);
 
     var y_scale = d3.scale.sqrt()
-        .range([height-margin, margin]) // margin-height to reverse direction
+        .range([height - margin, margin]) // margin-height to reverse direction
         .domain(y_extent);
 
-    var xfunc = function(d) {return x_scale(d[xval])};
-    var yfunc = function(d) {return y_scale(d[yval])};
+    var xfunc = function (d) { return x_scale(d[xval]); };
+    var yfunc = function (d) { return y_scale(d[yval]); };
 
-    var svg = document.getElementById('mainSVG')
+    var svg = document.getElementById('mainSVG');
     while (svg.lastChild) {
         svg.removeChild(svg.lastChild);
     }
@@ -71,32 +74,34 @@ function draw(data, xval, yval, xlab, ylab) {
     var y_axis = d3.svg.axis().scale(y_scale).orient('left');
 
     d3.select('svg')
-    .append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + (height-margin) + ')')
-    .call(x_axis);
+        .append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + (height - margin) + ')')
+        .call(x_axis);
 
     d3.select('svg')
-    .append('g')
-    .attr('class', 'y axis')
-    .attr('transform', 'translate(' + margin + ',0)')
-    .call(y_axis);
+        .append('g')
+        .attr('class', 'y axis')
+        .attr('transform', 'translate(' + margin + ',0)')
+        .call(y_axis);
 
     d3.select('.x.axis')
-    .append('text')
-    .text(xlab)
-    .attr('x', function(){return (width / 2) - margin})
-    .attr('y', margin/1.5);
+        .append('text')
+        .text(xlab)
+        .attr('x', function () { return (width / 2) - margin; })
+        .attr('y', margin / 1.5);
 
      d3.select('.y.axis')
-    .append('text')
-    .text(ylab)
-    .attr('transform', "rotate (-90, -30, 0) translate(-240)")
+        .append('text')
+        .text(ylab)
+        .attr('transform', "rotate (-90, -30, 0) translate(-240)");
 
     // pictures
     d3.select('#hackers')
         .selectAll('img')
-        .on('click', function(d) {selectHacker(d, data.links, data.nodes, xfunc, yfunc)}); 
+        .on('click', function (d) {
+            selectHacker(d, data.links, data.nodes, xfunc, yfunc);
+        });
 
     // nodes
     drawNodes(data, xfunc, yfunc);
@@ -107,27 +112,27 @@ function draw(data, xval, yval, xlab, ylab) {
 
 function selectHacker(hacker, links, nodes, xfunc, yfunc) {
 
-    nodes.forEach(function(n) {
-        n.current = (n.id == hacker.id);
-        });
+    nodes.forEach(function (n) {
+        n.current = (n.id === hacker.id);
+    });
 
     var id = hacker.id;
 
     var filteredlinks = links.filter(
-            function(l) {return l.source == id || l.target == id}
-            );
+            function (l) { return l.source === id || l.target === id; }
+        );
 
     drawLinks(filteredlinks, nodes, xfunc, yfunc);
     reDrawNodes({'nodes': nodes, 'links': links}, xfunc, yfunc);
 
     var hackerIDs = _.union(
-            filteredlinks.map(function(l) {return l.source}),
-            filteredlinks.map(function(l) {return l.target})
-            );
+            filteredlinks.map(function (l) { return l.source; }),
+            filteredlinks.map(function (l) { return l.target; })
+        );
 
     hackerIDs = [id].concat(_.without(hackerIDs, id));
 
-    var hackers = _.map(hackerIDs, function(n) {return nodes[n]});
+    var hackers = _.map(hackerIDs, function (n) { return nodes[n]; });
 
     d3.select('#collaborators')
         .selectAll('div')
@@ -136,26 +141,26 @@ function selectHacker(hacker, links, nodes, xfunc, yfunc) {
         .remove();
 
     var divs = d3.select('#collaborators')
-        .selectAll('div')
-        .data(hackers)
-        .enter()
-        .append('div')
-        .attr('class', 'collaborator')
-        .attr('height', '40px')
-        .attr('width', '200px')
+            .selectAll('div')
+            .data(hackers)
+            .enter()
+            .append('div')
+            .attr('class', 'collaborator')
+            .attr('height', '40px')
+            .attr('width', '200px');
 
-        divs.append('img')
+    divs.append('img')
         .attr('class', 'collaborator-image')
         .attr('height', '30px')
         .attr('width', '30px')
         .attr('vertical-align', 'center')
-        .attr('src', function(d) {return d.avatar_url})
-        .attr('title', function(d) {return d.login})
+        .attr('src', function (d) { return d.avatar_url; })
+        .attr('title', function (d) { return d.login; });
 
-        divs.append('div')
+    divs.append('div')
         .attr('class', 'collaborator-name')
         .append('span')
-        .text(function(d) {return d.login});
+        .text(function (d) { return d.login; });
 
 }
 
@@ -173,15 +178,15 @@ function drawLinks(links, nodes, xfunc, yfunc) {
         .data(links)
         .enter()
         .append('svg:line')
-        .attr('x1', function(d) {return xfunc(nodes[d.source])})
-        .attr('y1', function(d) {return yfunc(nodes[d.source])})
-        .attr('x2', function(d) {return xfunc(nodes[d.target])})
-        .attr('y2', function(d) {return yfunc(nodes[d.target])})
+        .attr('x1', function (d) { return xfunc(nodes[d.source]); })
+        .attr('y1', function (d) { return yfunc(nodes[d.source]); })
+        .attr('x2', function (d) { return xfunc(nodes[d.target]); })
+        .attr('y2', function (d) { return yfunc(nodes[d.target]); })
         .style('stroke', 'rgb(0, 52, 72)')
         .style('alpha', 1)
         .style('stroke-width', 1);
 
-} 
+}
 
 
 function drawNodes(data, xfunc, yfunc) {
@@ -191,13 +196,15 @@ function drawNodes(data, xfunc, yfunc) {
         .data(data.nodes)
         .enter()
         .append('circle')
-        .attr('cx', function(d) {return xfunc(d)})
-        .attr('cy', function(d) {return yfunc(d)})
-        .attr('r', function(d) {return 6})
-        .attr('class', function(d) {return 'notselected'})
-        .on('click', function(d) {selectHacker(d, data.links, data.nodes, xfunc, yfunc)})
+        .attr('cx', function (d) { return xfunc(d); })
+        .attr('cy', function (d) { return yfunc(d); })
+        .attr('r', function (d) { return 6; })
+        .attr('class', function (d) { return 'notselected'; })
+        .on('click', function (d) {
+            selectHacker(d, data.links, data.nodes, xfunc, yfunc);
+        })
         .append("svg:title")
-        .text(function(d) { return d.login; });
+        .text(function (d) { return d.login; });
 
 }
 
@@ -209,14 +216,15 @@ function reDrawNodes(data, xfunc, yfunc) {
         .data(data.nodes)
         .transition()
         .duration(1000)
-        .attr('cx', function(d) {return xfunc(d)})
-        .attr('cy', function(d) {return yfunc(d)})
-        .attr('r', function(d) {return 6})
-        .attr('class', function(d) {
+        .attr('cx', function (d) { return xfunc(d); })
+        .attr('cy', function (d) { return yfunc(d); })
+        .attr('r', function (d) { return 6; })
+        .attr('class', function (d) {
+            // should be simplified now error has been identified
             if (d.current) {
-               return 'selected'
+                return 'selected';
             }
-           return 'notselected'
+            return 'notselected';
         });
 
 }
