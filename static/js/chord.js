a7b24c20d372e651f3759dfad9455bbb68328d74
Nick Collins <nick@vividx.co.uk>
2013-07-06 17:31:19 +0100

    refactoring

--- static/js/chord.js
+++ static/js/chord.js
@@ -6,54 +6,21 @@ function setup_chord(data) {
 
     var id, filteredlinks, selected_hackers, matrix;
 
-    // base data:
+    // BASE DATA
+
     var hackers = data.nodes.map(function (d) {
         d.selected = false;
         return d;
     });
 
-    // sample matrix, the group centered on Zach
-    matrix = [];
-    id = 3; // Zach Allaun
+    _.each([52,31,43,8,29,16,11,1,3], function (i) {
+        hackers[i].selected = true;
+    });
 
-    filteredlinks = data.links.filter(
-            function (l) { return l.source === id || l.target === id; }
-            );
-
-    selected_hackers = _.union(
-            _.map(filteredlinks, function (l) { return l.source; }),
-            _.map(filteredlinks, function (l) { return l.target; })
-            );
-
-    for (var i=0; i< selected_hackers.length; i++) {
-        hackers[selected_hackers[i]].selected = true;
-    }
-
-    var index = {};
-    for (var i = 0; i < selected_hackers.length; i++) {
-        matrix[i] = [];
-        index[selected_hackers[i]] = i;
-        for (var j = 0; j < selected_hackers.length; j++) {
-            matrix[i].push(0);
-        }
-    }
-
-    _.each(data.links,
-            function (l) {
-                if (_.contains(selected_hackers, l.source) && _.contains(selected_hackers, l.target)) {
-                    matrix[index[l.source]][index[l.target]] = l.weight;
-                    matrix[index[l.target]][index[l.source]] = l.weight;
-                }
-            }
-          );
-
-    var chord = d3.layout.chord()
-        .padding(.05)
-        .sortSubgroups(d3.descending)
-        .matrix(matrix);
+    // SETUP
 
     var width = 1000,
-        height = 500,
+        height = 600,
         innerRadius = Math.min(width, height) *  .3, //.41,
         outerRadius = innerRadius * 1.1;
 
@@ -67,6 +34,69 @@ function setup_chord(data) {
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
+    d3.select("#mainSVG")
+        .append("defs")
+        .selectAll("pattern")
+        .data(hackers)
+        .enter()
+        .append("pattern")
+        .attr("id", function (d) { return "avatar-" + d.login; })
+        .attr("height", 30)
+        .attr("width", 30)
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("patternUnits", "userSpaceOnUse")
+        .attr("patternUnits", "objectBoundingBox")
+        .append("image")
+        .attr("height", 30)
+        .attr("width", 30)
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("xlink:href", function (d) {
+            return d.avatar_url;
+        });
+
+
+
+    selected_hackers = _.filter(hackers, function (d) { return d.selected });
+
+    function connection_matrix(selected_hackers) {
+        var matrix = [];
+
+        var index = {};
+        for (var i = 0; i < selected_hackers.length; i++) {
+            matrix[i] = [];
+            index[selected_hackers[i].id] = i;
+            for (var j = 0; j < selected_hackers.length; j++) {
+                matrix[i].push(0);
+            }
+        }
+
+        _.each(data.links,
+                function (l) {
+                    var selected_ids = _.map(selected_hackers, function (d) { return d.id });
+                    if (_.contains(selected_ids, l.source) && _.contains(selected_ids, l.target)) {
+                        matrix[index[l.source]][index[l.target]] = l.weight;
+                        matrix[index[l.target]][index[l.source]] = l.weight;
+                    }
+                }
+              );
+
+        return matrix;
+    }
+
+    matrix = connection_matrix(selected_hackers);
+
+    /*
+       var chord = d3.layout.chord()
+       .padding(.05)
+       .sortSubgroups(d3.descending)
+       .matrix(matrix);
+       */
+
+    var matrix2chord = d3.layout.chord().padding(.05).sortSubgroups(d3.descending).matrix,
+        chord = matrix2chord(matrix);
+
     svg.append("g")
         .attr("class", "groups")
         .selectAll("g")
@@ -78,27 +108,26 @@ function setup_chord(data) {
         .style("fill", function(d) { return fill(d.index); })
         .style("stroke", function(d) { return fill(d.index); })
         .attr("d", d3.svg.arc().innerRadius(innerRadius).outerRadius(outerRadius))
-        .attr("id", function (d) { return "group" + d.index + "-" + j; })
+        .attr("id", function (d) { return "group" + d.index /*+ "-" + j*/; })
         .on("mouseover", fade(.1))
         .on("mouseout", fade(1));
+
+    var total = _.reduce(chord.groups(), function (acc, d) {return acc + d.value}, 0);
 
     svg.selectAll(".group")
         .append("svg:text")
         .attr("x", 6)
         .attr("dy", 15)
         .filter(function (d) {
-            //console.log(d);
-            return d.value > 60;
+            //return true;
+            return d.value > total / 6;
         })
     .append("svg:textPath")
-        .attr("xlink:href", function (d) { return "#group" + d.index + "-" + j; })
+        .attr("xlink:href", function (d) { return "#group" + d.index /*+ "-" + j*/; })
         .text(function (d) {
-            //return "hello"
-            return data.nodes[selected_hackers[d.index]].login;
+            //console.log('test');
+            return selected_hackers[d.index].login
         });
-
-
-
 
     // Returns an array of tick angles and labels, given a group.
     function groupTicks(d) {
@@ -121,7 +150,6 @@ function setup_chord(data) {
         };
     }
 
-
     svg.append("g")
         .attr("class", "chords")
         .selectAll("path")
@@ -132,95 +160,64 @@ function setup_chord(data) {
         .style("fill", function(d) { return fill(d.target.index); })
         .style("opacity", 1);
 
+    // selection callback
+
+    function select(d) {
+        d.selected = true;
+        redraw();
+    }
+
+    function redraw() {
+        var selected_hackers = _.filter(hackers, function (d) { return d.selected }),
+            matrix = connection_matrix(selected_hackers),
+            chord = matrix2chord(matrix);
+    }
+
     // AVATARS
 
-    d3.select("#mainSVG")
-        .append("defs")
-        .selectAll("pattern")
-        //.data(chord.groups)
-        .data(hackers)
-        .enter()
-        .append("pattern")
-        //.attr("id", function (d) { return "avatar" + d.index; })
-        .attr("id", function (d) { return "avatar-" + d.login; })
-        .attr("height", 30)
-        .attr("width", 30)
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("patternUnits", "userSpaceOnUse")
-        .attr("patternUnits", "objectBoundingBox")
-        .append("image")
-        .attr("height", 30)
-        .attr("width", 30)
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("xlink:href", function (d) {
-            //return data.nodes[selected_hackers[d.index]].avatar_url;
-            return d.avatar_url;
-        });
-
-
-    function layout_avatars(groups) {
-
-        groups.forEach(function (d) {
-            d.login = hackers[selected_hackers[d.index]].login;
-            d.selected = true;
-        });
-
-        var unselected = hackers.filter(function (d) { return !d.selected; });
+    function layout_avatars(hackers, groups) {
+        // takes the array of hackers, either with "selected"
+        // true or false and the chord groups and adds "x" and "y"
+        // coordinates.
+        var selected = _.filter(hackers, function (d) { return d.selected }),
+            unselected = hackers.filter(function (d) { return !d.selected; });
 
         for (var i=0; i < unselected.length; i++) {
-            unselected[i].col = i % 4;
-            unselected[i].row = Math.floor(i / 4);
-            unselected[i].selected = false;
+            var col = i % 4,
+                row = Math.floor(i / 4);
+            unselected[i].x = 20 + col * 40 - width / 2;
+            unselected[i].y = 20 + row * 40 - height / 2;
         }
 
-        function x_pos (d) {
-            var center_x, angle, rotate, x_mod;
-            if (d.selected) {
-                center_x = width / 2;
-                angle = (d.startAngle + d.endAngle) / 2;
-                rotate = - Math.PI / 2;
-                x_mod = 1.2 * outerRadius * Math.cos(rotate + angle);
-            }
-            else {
-                x_mod = 20 + d.col * 40 - width / 2;
-            }
-            return x_mod;
-        }
+        var center_x = width / 2,
+            angle,
+            rotate = - Math.PI / 2;
 
-        function y_pos (d) {
-            var center_y, angle, rotate, y_mod;
-            if (d.selected) {
-                center_y = height / 2;
-                angle = (d.startAngle + d.endAngle) / 2;
-                rotate = - Math.PI / 2;
-                y_mod = 1.2 * outerRadius * Math.sin(rotate + angle);
-            }
-            else {
-                y_mod = 20 + d.row * 40 - height / 2;
-            }
-            return y_mod;
+        for (var i=0; i < selected.length; i++) {
+            angle = (groups[i].startAngle + groups[i].endAngle) / 2;
+            selected[i].x = 1.2 * outerRadius * Math.cos(rotate + angle);
+            selected[i].y = 1.2 * outerRadius * Math.sin(rotate + angle);
         }
+    }
+
+    function plot_avatars(groups) {
+
+        layout_avatars(hackers, groups)
 
         svg.selectAll(".mugshots")
-            //.data(groups)
-            .data(groups.concat(unselected))
+            .data(hackers)
             .enter()
             .append("svg:circle")
-            .attr("cx", x_pos)
-            .attr("cy", y_pos)
+            .attr("cx", function (d) {return d.x})
+            .attr("cy", function (d) {return d.y})
             .attr("r", 15)
             .attr("stroke", "black")
             .attr("stroke-width", 2)
             .attr("fill", function (d) {
-                //return "url(#avatar" + d.index + ")";
-                //return "url(#avatar-" + hackers[d.mainIndex].login + ")";
                 return "url(#avatar-" + d.login + ")";
             });
-
     }
 
-    layout_avatars(chord.groups());
+    plot_avatars(chord.groups());
 
 }
